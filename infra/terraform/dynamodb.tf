ec176c5f792246bf34469b1687cba5530321060b
genildoburgos <gnldburgos@gmail.com>
2026-02-11 14:03:50 -0300

    feat(terraform): fixa capacidade de leitura e escrita de tabelas dynamodb

--- infra/terraform/dynamodb.tf
+++ infra/terraform/dynamodb.tf
@@ -2,6 +2,8 @@ resource "aws_dynamodb_table" "api_table" {
     name = var.dynamodb_table_name
     hash_key = "id"
     billing_mode = "PAY_PER_REQUEST"
+    read_capacity = 5
+    write_capacity = 5
 
     attribute {
         name = "id"
