92bd19897799d6621ecc7e3be0b9736101e0828b
genildoburgos <gnldburgos@gmail.com>
2026-02-12 01:07:16 -0300

    feat(terraform): cria tabela de user e imagem

--- infra/terraform/dynamodb.tf
+++ infra/terraform/dynamodb.tf
@@ -1,5 +1,5 @@
-resource "aws_dynamodb_table" "api_table" {
-    name = var.dynamodb_table_name
+resource "aws_dynamodb_table" "users" {
+    name = var.table_users
     hash_key = "id"
     billing_mode = "PROVISIONED"
     read_capacity = 5
@@ -8,5 +8,58 @@ resource "aws_dynamodb_table" "api_table" {
     attribute {
         name = "id"
         type = "S"
+    }
+
+    attribute {
+        name = "email"
+        type = "S"
+    }
+
+    global_secondary_index {
+        name            = "EmailIndex"
+        hash_key        = "email"
+        write_capacity  = 5
+        read_capacity   = 5
+        projection_type = "ALL"
+    }
+
+    tags = {
+        Project = "gb-core"
+    }
+}
+
+resource "aws_dynamodb_table" "images" {
+    name = var.table_images
+    hash_key = "id"
+    billing_mode = "PROVISIONED"
+    read_capacity = 5
+    write_capacity = 5
+
+    attribute {
+        name = "id"
+        type = "S"
+    }
+
+    attribute {
+        name = "userId"
+        type = "S"
+    }
+
+    attribute {
+        name = "createdAt"
+        type = "N"
+    }
+
+    global_secondary_index {
+        name            = "UserIndex"
+        hash_key        = "userId"
+        range_key       = "createdAt"
+        write_capacity  = 5
+        read_capacity   = 5
+        projection_type = "ALL"
+    }
+
+    tags = {
+        Project = "gb-core"
     }
 }
