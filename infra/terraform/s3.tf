3de9090f2abd54c7aeaa0003e2bc18b57773533e
genildoburgos <gnldburgos@gmail.com>
2026-02-21 13:17:21 -0300

    feat(terraform): adiciona configuracao de politica de acesso publico para bucket s3

--- infra/terraform/s3.tf
+++ infra/terraform/s3.tf
@@ -8,3 +8,33 @@ resource "aws_s3_bucket_ownership_controls" "api_bucket_ownership" {
         object_ownership = "BucketOwnerPreferred"
     }
 }
+
+resource "aws_s3_bucket_public_access_block" "api_bucket_public_access" {
+    bucket = aws_s3_bucket.api_bucket.id
+
+    block_public_acls       = false
+    block_public_policy     = false
+    ignore_public_acls      = false
+    restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "public_read" {
+    bucket = aws_s3_bucket.api_bucket.id
+
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Sid = "PublicReadGetObject"
+                Effect = "Allow"
+                Principal = "*"
+                Action = ["s3:GetObject"]
+                Resource = "${aws_s3_bucket.api_bucket.arn}/*"
+            }
+        ]
+    })
+
+    depends_on = [
+        aws_s3_bucket_public_access_block.api_bucket_public_access
+    ]
+}
